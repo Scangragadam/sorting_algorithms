6730664df57df97a4bf413359be73eccf7fde2c5
Younrag <younes.raguebi@usmba.ac.ma>
2023-11-24 21:45:31 +0100

    Younes

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -1,6 +1,6 @@
 /*
  * File: 106-bitonic_sort.c
- * Auth: Omar Tazi
+ * Auth: Younes Raguebi
  */
 
 #include "sort.h"
@@ -44,7 +44,7 @@ void bitonic_merge(int *array, size_t size, size_t start, size_t seq,
 		{
 			if ((flow == UP && array[i] > array[i + jump]) ||
 			    (flow == DOWN && array[i] < array[i + jump]))
-				swap_ints(array + i, array + i + jump);
+				swap_func(array + i, array + i + jump);
 		}
 		bitonic_merge(array, size, start, jump, flow);
 		bitonic_merge(array, size, start + jump, jump, flow);
